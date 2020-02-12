c6c2e2fbf05dc7170cb12da325a1b004cfbe69e9
selmar meneses <menesesselmar@gmail.com>
2020-02-12 00:31:38 -0400

    rutas con laravel

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,28 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/usuarios', function() {
+    return "usuarios";
+});
+
+Route::get('/usuarios/{id}', function($id) {
+    return "mostrando detalle del usuario: {$id}";
+})->where('id', '[0-9]+');
+
+Route::get('usuarios/nuevo', function() {
+    return "crear nuevo usuario";
+});
+
+Route::get('/saludo/{name}/{nickname?}', function($name, $nickname= null) {
+    if($nickname){
+        return "hola {$name}, tu apodo es {$nickname}";
+    }
+    else{
+        return "hola {$name}, no tienes apodo";
+    }
+    
 });
